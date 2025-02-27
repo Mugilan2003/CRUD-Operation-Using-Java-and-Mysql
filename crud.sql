9e44351948fbe00f858920123c13f8fc211bef24
Mugilan A <136972673+Mugilan2003@users.noreply.github.com>
2025-02-27 12:00:39 +0530

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,11 @@
+use crudopp;
+create table crud(
+Id Int Primary Key,
+Name Varchar(30),
+Mark Int,
+City varchar(50)
+);
+describe crud;
+Select*from crud;
+Alter table crud modify column Id integer Auto_Increment;
+Insert Into crud values(5,"Ramesh",466,"Erode");

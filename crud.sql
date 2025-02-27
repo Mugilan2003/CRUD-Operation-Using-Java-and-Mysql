084936fac39958a9b3d0248eebd69d362efe9aa5
Mugilan A <136972673+Mugilan2003@users.noreply.github.com>
2025-02-27 12:11:37 +0530

    Update crud.sql

--- crud.sql
+++ crud.sql
@@ -1,10 +1,10 @@
 use crudopp;
 create table crud(
-Id Int Primary Key,
+Id Int Primary Key Auto_Increment,
 Name Varchar(30),
 Mark Int,
 City varchar(50)
 );
 describe crud;
 Select*from crud;
-Alter table crud modify column Id integer Auto_Increment;
+
